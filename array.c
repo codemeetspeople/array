1b269a0e282cc55be72cdaecdf85a6c53df37e2e
caiman <me@caiman.dev>
2021-10-30 19:57:47 +0300

    Revert "[master] Create arrayPrint function"
    This reverts commit 6e700747a2170b6d944833f4b722ca4d6c2cdc8a.

--- array.c
+++ array.c
@@ -2,20 +2,14 @@
 
 #define SIZE 10
 
-void arrayPrint(int array[], int size) {
-    int last = size - 1;
+int main() {
+    int array[SIZE] = {1,2,3,4,5,6,7,8,9,10};
+    int last = SIZE - 1;
 
     for ( int i = 0; i < last; i++ ) {
         printf("%d ", array[i]);
     }
     printf("%d\n", array[last]);
-}
-
-
-int main() {
-    int array[SIZE] = {1,2,3,4,5,6,7,8,9,10};
-
-    arrayPrint(array, size);
 
     return 0;
 }
