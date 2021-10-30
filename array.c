30880d14731903e821b30581d1b58fbf703c9a92
caiman <me@caiman.dev>
2021-10-30 20:02:21 +0300

    [array_increment] Add array increment function

--- array.c
+++ array.c
@@ -11,11 +11,18 @@ void arrayPrint(int array[], int size) {
     printf("%d\n", array[last]);
 }
 
+void arrayIncrement(int array[], int size) {
+    for ( int i = 0; i < size; i++ ) {
+        array[i] += 1;
+    }
+}
 
 int main() {
     int array[SIZE] = {1,2,3,4,5,6,7,8,9,10};
 
-    arrayPrint(array, size);
+    arrayPrint(array, SIZE);
+    arrayIncrement(array, SIZE);
+    arrayPrint(array, SIZE);
 
     return 0;
 }
